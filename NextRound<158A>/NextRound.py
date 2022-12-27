2df150f1e97243233899c43412f017f86f171722
Patrick Sloan <patrickbarrettsloan@outlook.com>
2022-12-27 19:50:25 +0000

    Added Documentation to NextRound

--- NextRound<158A>/NextRound.py
+++ NextRound<158A>/NextRound.py
@@ -1,22 +1,21 @@
 # https://codeforces.com/problemset/problem/158/A
 
-# n = number of partisipants 
-# k = place
-
-
 n, k = map(int, input().split(' '))
-a = input().split(' ')
-num = sorted(a)[k]
-print(a)
-
+a = list(map(int, input().split(' ')))
 count = 0
 
 for i in range(len(a)):
-    print(i)
-    if i > a[k]:
+    if a[i] >= a[k-1] and a[i] != 0:
         count += 1
-print(count)
+print(count)   
 
 '''
-need to get the value at a[k]
+This one was a bit of a ball ache at first.
+Firstly we deal with the first two inputs mapping the split of the input values to two variables.
+The second input on the next line is mapped to a list variable again split by spaces.
+What can be considered the main part of our program begins with a for loop, that iterates over the
+length of a.
+An if statement then checks to see each item in a is greater than or equal to the element in 
+position k.
+As long as the value of a[i] is not 0 the program will increase the count by 1.
 '''
