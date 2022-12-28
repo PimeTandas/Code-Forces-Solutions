6a7a5b5a97cd662acfb596d4af6925af70a1e2c7
Patrick Sloan <patrickbarrettsloan@outlook.com>
2022-12-28 14:00:51 +0000

    Added some challenges and doc

---
+++ Elephant<617A>/Elephant.py
@@ -0,0 +1,26 @@
+# https://codeforces.com/problemset/problem/617/A
+
+x = int(input())
+count = 0
+
+while x != 0:
+    if x > 4:
+        x -= 5
+        count += 1
+    elif x > 3:
+        x -= 4
+        count += 1
+    elif x > 2:
+        x -= 3
+        count += 1
+    elif x > 1:
+        x -= 2
+        count += 1
+    elif x > 0:
+        x -=1
+        count += 1
+print(count)
+
+'''
+
+'''
