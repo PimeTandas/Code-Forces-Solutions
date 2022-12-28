87af029e00cd891c2fe28057c9ade84c3e11fa7b
Patrick Sloan <patrickbarrettsloan@outlook.com>
2022-12-28 19:34:46 +0000

    Wrong Subtraction challege and doc

---
+++ WrongSubtraction<977A>/WrongSubtraction.py
@@ -0,0 +1,18 @@
+# https://codeforces.com/problemset/problem/977/A
+
+x, y = map(int, input().split(' '))
+
+for i in range(y):
+    if x % 10 != 0:
+        x = x - 1
+    elif x % 10 == 0:
+        x = x // 10
+print(x)
+
+'''
+Starts by taking input splitting it and mapping the values to two int variables.
+Next uses a for loop to loop over the amount of subtractions we are going to be making.
+Finally we check the two conditions stated in the breif and execute the subtraction accordingly.
+Printing the end value after y amount of subtractions to our original number.
+'''
+
